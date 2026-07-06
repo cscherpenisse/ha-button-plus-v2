5c50f71d0781f61cbfebd9f35f0bf922f830944b
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-07-06 18:49:36 +0200

    Add constants for manufacturer and topics in const.py

--- custom_components/button_plus_v2/const.py
+++ custom_components/button_plus_v2/const.py
@@ -1,6 +1,31 @@
+from __future__ import annotations
+
 DOMAIN = "button_plus_v2"
 
-CONF_DEVICE_ID = "device_id"
+MANUFACTURER = "Button+"
+
+DEFAULT_PORT = 80
+
 CONF_HOST = "host"
 
-MQTT_BASE = "buttonplus"
+CONF_DEVICE_ID = "device_id"
+
+CONF_NAME = "name"
+
+BUTTON_TOPIC = "button"
+
+DISPLAY_TOPIC = "displayitem"
+
+LED_TOPIC = "led"
+
+SENSOR_TOPIC = "sensor"
+
+BRIGHTNESS_TOPIC = "brightness"
+
+CONFIG_ENDPOINT = "/config"
+
+PLATFORMS = (
+    "sensor",
+    "button",
+    "light",
+)
