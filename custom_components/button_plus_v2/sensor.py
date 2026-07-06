bb44b8959642fea2b3b1efc9e5312d1df1c3aafd
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-07-06 18:59:42 +0200

    Add type hints to ButtonPlusSensor initialization

--- custom_components/button_plus_v2/sensor.py
+++ custom_components/button_plus_v2/sensor.py
@@ -1,11 +1,35 @@
+from __future__ import annotations
+
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up sensors."""
+
+    coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
+    device_id = entry.data["device_id"]
+
+    entities = [
+        ButtonPlusSensor(coordinator, device_id, "sens1"),
+        ButtonPlusSensor(coordinator, device_id, "sens2"),
+    ]
+
+    async_add_entities(entities)
+
 
 class ButtonPlusSensor(SensorEntity):
+    """Button+ sensor."""
 
-    def __init__(self, coordinator, device_id, sensor_id):
+    def __init__(self, coordinator, device_id: str, sensor_id: str) -> None:
         self.coordinator = coordinator
         self.device_id = device_id
         self.sensor_id = sensor_id
+
         self._attr_name = f"Button+ {sensor_id}"
         self._attr_unique_id = f"{device_id}_{sensor_id}"
 
